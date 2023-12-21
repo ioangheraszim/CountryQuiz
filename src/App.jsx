937fc16654cffd33a3ee0bca98247f8191fc9513
ioangheraszim <gijohny17@gmail.com>
2023-12-21 23:53:37 +0000

    added colors and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold font-vietnam">Hello world!</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'vietnam' : ['Be Vietnam Pro', 'sans-serif']
+    },
+    color: {
+      gradient: 'linear-gradient(#E65895, #BC6BE8)',
+      skyblue: '#3E9FFF',
+      backgroundOne: '#393F6E',
+      backgroundTwo: '#343964',
+      milkWhite: '#E2E4F3',
+      redOrange:'#DD524C',
+      gray: '#8B8EAB',
+      mustard: '#FFECC8'
+    },
     extend: {},
   },
   plugins: [],
